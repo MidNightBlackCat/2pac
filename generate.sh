cfd3212b5f910ca6ac295dec272e7e82336f66aa
Li Mingda <583424001@qq.com>
2016-05-29 22:44:12 +0800

    Create generate.sh

---
+++ generate.sh
@@ -0,0 +1,9 @@
+
+./a.py
+cat gfwlist.conf |grep server > xxx.conf
+sed 's/server=\/.//g' xxx.conf > yyy.conf 
+sed 's/\/127.0.0.1#1053//g' yyy.conf > zzz.conf 
+awk < zzz.conf '{printf "  \""$i"\"\:1,\n"}' > aaa.conf
+sed '$s/.$//' aaa.conf > finish.conf
+
+rm -rf gfwlist* xxx.conf yyy.conf zzz.conf aaa.conf
